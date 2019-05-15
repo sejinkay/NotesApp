ae402d55c6083b71123996b3a0c39629360a2889
sejinkay <sejinkay@gmail.com>
2019-05-15 12:00:04 +0100

    renamed some test to keep consistency, more view test and all passes

--- tests/noteSpec.js
+++ tests/noteSpec.js
@@ -1,17 +1,17 @@
 (function(exports) {
-  function testText () {
+  function testNote () {
 var note = new Note();
 
     if (note.text !== "My favourite language is JavaScript.") {
       throw new Error("Wrong message displayed");
     }else {
-      console.log("Test passed!");
+      console.log("Note test passed!");
     }
 
     if (note.return() !== "My favourite language is JavaScript.") {
       throw new Error("Wrong message2 displayed");
     }else {
-      console.log("Test2 passed!");
+      console.log("Note test2 passed!");
     }
   };
 
@@ -21,20 +21,20 @@ var note = new Note();
     if (note.notes.length !== 0) {
       throw new Error("There are no array!");
     }else {
-      console.log("NoteList test passed!")
+      console.log("List test passed!")
     };
 
     if (note.return() !== note.notes) {
       throw new Error("There are no array!");
     }else {
-      console.log("NoteList test2 passed!")
+      console.log("List test2 passed!")
     };
 
     note.create("Favourite drink: seltzer")
     if (note.notes.includes("Favourite drink: seltzer") !== true) {
       throw new Error("There are no array!");
     }else {
-      console.log("NoteList test3 passed!")
+      console.log("List test3 passed!")
     };
   };
 
@@ -43,16 +43,29 @@ var note = new Note();
     var view = new NoteView();
     var htmlList = "<ul><li><div>Moody blues</div></li></ul><ul><li><div>Crash</div></li></ul>";
 
+    if (view.show(note) !== "") {
+      throw new Error("List and view doesn't match!");
+    }else {
+      console.log("View0 test passed!(no item)")
+    };
+
     note.create("Moody blues")
+
+    if (view.show(note) !== "<ul><li><div>Moody blues</div></li></ul>") {
+      throw new Error("List and view doesn't match!");
+    }else {
+      console.log("View1 test passed!(one item)")
+    };
+
     note.create("Crash")
 
     if (view.show(note) !== htmlList) {
       throw new Error("List and view doesn't match!");
     }else {
-      console.log("View test passed!")
+      console.log("View2 test passed!(several items)")
     };
   };
-    testText();
+    testNote();
     testList();
     testView();
 })(this);
