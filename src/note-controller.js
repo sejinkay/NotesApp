c65875d0217be36dbad24b198ef0276cfa3976f0
sejinkay <sejinkay@gmail.com>
2019-05-16 15:24:37 +0100

    now controller can display on the index what's in the noteList

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,9 +1,14 @@
 (function(exports) {
   function NoteController() {
+    this.noteList = new NoteList()
+    this.create = this.noteList.create("Favourite drink: seltzer")
+    this.noteListView = new NoteView()
   }
+  NoteController.prototype = {
+    displayNote: function() {
+      document.getElementById('app').innerHTML = this.noteListView.show(this.noteList);
+    }
+  }
+
   exports.NoteController = NoteController;
 })(this);
-
-var elem = document.getElementById('app');
-console.log(elem);
-elem.innerHTML = 'Howdy';
